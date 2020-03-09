f9a081c0f5b623e56fdd9f6a2f8d8ed4c7e1a06c
Warren Uhrich <hello@warren.codes>
2020-03-09 16:56:42 -0600

    Add li to ul.

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,20 @@ var activeList = document.querySelector( 'ul' );
 // Retrieve "Completed" list.
 var completedList = document.querySelector( 'ul:last-of-type' );
 
+// Retrieve the to-do input.
+var newTask = document.querySelector( '[name="new-task"]' );
+
 // Select our form.
 var form = document.querySelector( 'form' );
 // We listen to our form for a submission.
 form.addEventListener( 'submit', function ( event ) {
     // Prevent a page-refresh from a REAL form submission.
     event.preventDefault();
-    
-    
+    // Let's add the list item into our UL.
+    activeList.innerHTML += `
+        <li>
+            <input type="checkbox">
+            ` + newTask.value + `
+        </li>
+    `;
 } );
